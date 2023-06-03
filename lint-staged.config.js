7aa4fc327d6e837dfebe8e978075cc29e24355f2
Felipe Vieira <felipvieiraa@gmail.com>
2023-06-03 18:26:21 -0300

    chore: add husky and lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,10 @@
+export default {
+	'./src/**/*.(ts|tsx)': () => 'tsc --noEmit',
+
+	'./src/**/*.(tsx|ts)': (filenames) => [
+		`eslint --fix ${filenames.join(' ')}`,
+		`prettier --write ${filenames.join(' ')}`,
+	],
+
+	'*./**/*.(md|json)': (filenames) => `prettier --write ${filenames.join(' ')}`,
+};
